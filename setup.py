be092e8c7d9d4da06d913d5ba40b324d58b2ebf3
SysFAB <sysfab64@gmail.com>
2025-08-24 21:54:34 +0300

    init: init project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="osmxmpp",
+    version="0.1.0",
+
+    author="osmiumnet",
+    description="Python XMPP library",
+
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.10",
+)
