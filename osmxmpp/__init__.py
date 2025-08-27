a7c109c5e5424f8d6648841db224cce64667ea8b
SysFAB <sysfab64@gmail.com>
2025-08-28 00:29:01 +0300

    feat: move TLSFeature to tls_feature.py

--- osmxmpp/__init__.py
+++ osmxmpp/__init__.py
@@ -2,7 +2,9 @@ __version__ = "0.1.0"
 __author__ = "osmiumnet"
 
 from .client import XMPPClient
-from .feature import XMPPFeature, TLSFeature
+
+from .feature import XMPPFeature
+from .tls_feature import TLSFeature
 
 __all__ = [
     "XMPPClient",
