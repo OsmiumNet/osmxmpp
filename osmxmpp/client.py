b1c808d592ec6cb6a60700d783f0fba6b163f611
SysFAB <sysfab64@gmail.com>
2025-08-29 01:29:03 +0300

    feat: add _listen method and handlers

--- osmxmpp/client.py
+++ osmxmpp/client.py
@@ -14,7 +14,11 @@ class XMPPClient:
 
         self.handlers = {
             "connected": [],
+            "disconnected": [],
             "ready": [],
+            "message": [],
+            "presence": [],
+            "iq": [],
         }
 
 
@@ -22,10 +26,25 @@ class XMPPClient:
         self.handlers["connected"].append(handler)
         return handler
     
+    def on_disconnect(self, handler:Callable) -> Callable:
+        self.handlers["disconnected"].append(handler)
+        return handler
+    
     def on_ready(self, handler:Callable) -> Callable:
         self.handlers["ready"].append(handler)
         return handler
 
+    def on_message(self, handler:Callable) -> Callable:
+        self.handlers["message"].append(handler)
+        return handler
+    
+    def on_presence(self, handler:Callable) -> Callable:
+        self.handlers["presence"].append(handler)
+        return handler
+    
+    def on_iq(self, handler:Callable) -> Callable:
+        self.handlers["iq"].append(handler)
+        return handler
 
     def _start_xmpp_stream(self):
         stream_start = XMLElement(
@@ -51,6 +70,46 @@ class XMPPClient:
         data = self.socket.recv(4096)
         return XMLParser.parse_elements(data.decode("utf-8"))[0]
     
+    def _send_presence(self):
+        presence = XMLElement(
+            "presence",
+        )
+
+        self.socket.sendall(presence.to_string().encode("utf-8"))
+    
+
+    def _listen(self):
+        buffer = ""
+
+        while True:
+            data = self.socket.recv(4096)
+            if not data:
+                for handler in self.handlers["disconnected"]:
+                    handler()
+                break
+
+            buffer += data.decode("utf-8")
+
+            try:
+                elements = XMLParser.parse_elements(buffer)
+                buffer = ""
+            except Exception:
+                # Incomplete stanza
+                continue
+
+            for element in elements:
+                if element.name == "message":
+                    for handler in self.handlers["message"]:
+                        handler(element)
+
+                elif element.name == "presence":
+                    for handler in self.handlers["presence"]:
+                        handler(element)
+                
+                elif element.name == "iq":
+                    for handler in self.handlers["iq"]:
+                        handler(element)
+
 
     def connect(self, features:List[XMPPFeature]=None) -> None:
         if features is None:
@@ -81,5 +140,9 @@ class XMPPClient:
                     break
                 features_xml = new_features_xml
             
+            self._send_presence()
+
             for handler in self.handlers["ready"]:
                 handler()
+
+            self._listen()
