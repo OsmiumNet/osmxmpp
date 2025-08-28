dcd7b850a2453919429da91286a9ee071de64be9
SysFAB <sysfab64@gmail.com>
2025-08-29 00:49:04 +0300

    build: add PLAINMechanism to module exports

--- osmxmpp/__init__.py
+++ osmxmpp/__init__.py
@@ -5,11 +5,15 @@ from .client import XMPPClient
 
 from .feature import XMPPFeature
 from .tls_feature import TLSFeature
-from .sasl_feature import SASLFeature
+from .sasl_feature import SASLFeature, PLAINMechanism
 
 __all__ = [
     "XMPPClient",
+
     "XMPPFeature",
+
     "TLSFeature",
+
     "SASLFeature",
+    "PLAINMechanism"
 ]
