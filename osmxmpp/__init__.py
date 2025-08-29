508d1f1055e400b6a30e43a3129df11b8e8fd3d4
SysFAB <sysfab64@gmail.com>
2025-08-29 05:25:28 +0300

    refactor: rename features/sasl_feature.py to features/sasl.py

--- osmxmpp/__init__.py
+++ osmxmpp/__init__.py
@@ -8,7 +8,7 @@ from .client import XMPPClient
 
 from .features.abc import XMPPFeature
 from .features.tls_feature import TLSFeature
-from .features.sasl_feature import SASLFeature, PLAINMechanism
+from .features.sasl import SASLFeature, PLAINMechanism
 from .features.bind import BindFeature
 
 __all__ = [
--- osmxmpp/features/__init__.py
+++ osmxmpp/features/__init__.py
@@ -1,6 +1,6 @@
 from .abc import XMPPFeature
 from .tls_feature import TLSFeature
-from .sasl_feature import SASLFeature, PLAINMechanism
+from .sasl import SASLFeature, PLAINMechanism
 from .bind import BindFeature
 
 __all__ = [
