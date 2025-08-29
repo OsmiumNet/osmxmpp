c680a67b6746adc8dd45c635d4a7d52f7208da15
JustNevi <slavkad25@gmail.com>
2025-08-29 16:44:43 +0200

    fix: connect features

--- osmxmpp/client.py
+++ osmxmpp/client.py
@@ -299,7 +299,7 @@ class XMPPClient:
         """
 
         for feature_with_permissions in features_with_permissions:
-            self.connect_feature(feature_with_permissions[0], feature_with_permissions[1]) 
+            self.connect_feature(self, feature_with_permissions[0], feature_with_permissions[1]) 
 
 
     def connect_extension(self, extension:XMPPExtension, permissions: List[XMPPPermission] | XMPPPermission.ALL) -> None:
