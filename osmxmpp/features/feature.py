ebf8764a1b3cdf1fe229e378dbbe69311e55e129
SysFAB <sysfab64@gmail.com>
2025-08-29 04:37:24 +0300

    docs: document XMPPFeature

--- osmxmpp/features/feature.py
+++ osmxmpp/features/feature.py
@@ -1,6 +1,15 @@
 from abc import ABC, abstractmethod
 
 class XMPPFeature(ABC):
+    """
+    Features are used to implement specific XMPP stream features.
+
+    Attributes:
+        id (str): The ID of the feature implementation.
+        tag (str): The tag of the feature. This is used to identify the feature in the XML stream.
+        receive_new_features (bool): If client should receive new features after processing the current one.
+    """
+
     id = None
     tag = None
 
@@ -8,8 +17,20 @@ class XMPPFeature(ABC):
     
     @abstractmethod
     def connect_ci(self, ci) -> None:
+        """
+        Connects the feature to the client interface.
+
+        Args:
+            ci (XMPPCI): The client interface.
+        """
         ...
     
     @abstractmethod
     def process(self, element) -> None:
+        """
+        Processes the feature.
+
+        Args:
+            element (XMLElement): The XML element to process.
+        """
         ...
