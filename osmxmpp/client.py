9e02fba94504ced61ded28c1ed3257f4b6302368
JustNevi <slavkad25@gmail.com>
2025-08-29 16:50:14 +0200

    fix: connect extensions

--- osmxmpp/client.py
+++ osmxmpp/client.py
@@ -333,7 +333,7 @@ class XMPPClient:
         """
 
         for extension_with_permissions in extensions_with_permissions:
-            self.connect_extension(extension_with_permissions[0], extension_with_permissions[1])
+            self.connect_extension(self, extension_with_permissions[0], extension_with_permissions[1])
 
 
     def connect(self) -> None:
