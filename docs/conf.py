3380debba2b0619b5c5620a08184882174d8693a
SysFAB <sysfab64@gmail.com>
2025-09-06 17:08:55 +0300

    docs: connect extensions

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,11 @@ author = 'OsmiumNet'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",  # for Google/NumPy style docstrings
+    "sphinx_autodoc_typehints",
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
