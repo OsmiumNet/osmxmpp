06467209e9b354bf8864fdcc27b34264d8b58114
SysFAB <sysfab64@gmail.com>
2025-09-14 20:51:56 +0300

    fix: import service discovery in __init__.py

--- osmxmpp/extensions/__init__.py
+++ osmxmpp/extensions/__init__.py
@@ -1,7 +1,9 @@
 from .abc import XmppExtension
 from .omemo import OmemoExtension 
+from .service.discovery import ServiceDiscoveryExtension
 
 __ALL__ = [
     "XmppExtension",
     "OmemoExtension",
+    "ServiceDiscoveryExtension",
 ]
