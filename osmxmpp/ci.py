d24d1ec227cbc04b9c0cffac26b6990bb0f03e96
SysFAB <sysfab64@gmail.com>
2025-09-14 21:45:46 +0300

    fix: fix typo

--- osmxmpp/ci.py
+++ osmxmpp/ci.py
@@ -77,7 +77,7 @@ class XmppClientInterface:
         """
         self.__handle_permission(XmppPermission.SEND_XML)
         return self.__client._send_xml(xml)
-    o
+    
     def recv_xml(self) -> XmlElement:
         """
         Receives an XML element from the XMPP client.
