20cf314c0f49a6d274f22821e2c412a4c9af7d50
SysFAB <sysfab64@gmail.com>
2025-09-14 20:46:34 +0300

    build: update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="osmxmpp",
-    version="0.1.0",
+    version="1.0.0",
 
     author="osmiumnet",
     description="Python XMPP library",
